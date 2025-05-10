4a7366353bb0cf332a87c701b9ecac10212733a8
Julio Syah Putra <juliosp2107@gmail.com>
2025-05-10 18:02:26 +0700

    fix: next issues double html tags

--- src/app/(landingpage)/home/layout.js
+++ src/app/(landingpage)/home/layout.js
@@ -2,11 +2,5 @@ import QueryProvider from '@/components/QueryClientProvider';
 import '../../globals.css';
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <QueryProvider>{children}</QueryProvider>
-      </body>
-    </html>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 }
